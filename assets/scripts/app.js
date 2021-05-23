b5f0c3b5d1a7bc386bc186b20c50493f44c10b72
leandromuzzupappa <leann.muzzu@gmail.com>
2021-05-23 05:21:02 -0300

    Proyecto inicializado 🙌
    Mentira, ya lo empece hace como 8 horas

---
+++ assets/scripts/app.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+    let menuMobileBtn = document.querySelector(".menuMobile__btn");
+    menuMobileBtn.addEventListener("click", () => {
+        alert(1);
+    });
+});
