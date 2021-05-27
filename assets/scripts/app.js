e249b0fbac3e5f4938751f2f0534c2cc2b3d8021
leandromuzzupappa <leann.muzzu@gmail.com>
2021-05-26 21:15:39 -0300

    responsive casi terminado

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,11 @@
 window.addEventListener("load", () => {
     let menuMobileBtn = document.querySelector(".menuMobile__btn");
-    menuMobileBtn.addEventListener("click", () => {
-        alert(1);
-    });
+    if (menuMobileBtn) {
+        let menuMobile = document.querySelector(".menuMobile");
+
+        menuMobileBtn.addEventListener("click", () => {
+            menuMobileBtn.classList.toggle("menuMobile__btn--open");
+            menuMobile.classList.toggle("menuMobile--open");
+        });
+    }
 });
