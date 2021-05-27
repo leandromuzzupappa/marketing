46da8f275a35176db6b438374893f83aa64b77b7
leandromuzzupappa <leann.muzzu@gmail.com>
2021-05-27 00:19:50 -0300

    Se implemento slider y modificaciones de estilos varias

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,28 @@
+const HTML = document.querySelector("html");
+
 window.addEventListener("load", () => {
+    const header = document.querySelector(".header");
     let menuMobileBtn = document.querySelector(".menuMobile__btn");
     if (menuMobileBtn) {
-        let menuMobile = document.querySelector(".menuMobile");
-
         menuMobileBtn.addEventListener("click", () => {
-            menuMobileBtn.classList.toggle("menuMobile__btn--open");
-            menuMobile.classList.toggle("menuMobile--open");
+            header.classList.toggle("menuMobile__open");
+
+            if (header.classList.contains("menuMobile__open")) {
+                HTML.style.overflow = "hidden";
+            } else {
+                HTML.style.overflow = "inherit";
+            }
+        });
+    }
+
+    let pepitos = document.querySelector(".pepitos");
+    if (pepitos) {
+        let slider = new Swiper(pepitos, {
+            loop: true,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
         });
     }
 });
